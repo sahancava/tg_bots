cbf5d6bcc8eaa29e48f8917c5c0fae5502c796fd
Şahan <57447079+sahancava@users.noreply.github.com>
2023-06-04 02:00:11 +0300

    a

--- presale.py
+++ presale.py
@@ -2,7 +2,6 @@ from quart import Quart, request, jsonify
 from helpers import (create_mysql_connection, sendMessage, handle_config)
 import telegram
 import requests
-from bs4 import BeautifulSoup
 from time import sleep
 from selenium import webdriver
 from datetime import datetime
@@ -50,93 +49,54 @@ async def get_html(url):
     PRESALE_ADDRESS = None
 
     if response.status_code == 200:
-        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-        driver.get(url)
-        
-        try:
-            WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 'ant-card-body')))
-            parent_div = driver.find_element(By.CLASS_NAME, 'ant-card-body')
-            WebDriverWait(parent_div, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 'table-container')))
-            table_container_div = parent_div.find_element(By.CLASS_NAME, 'table-container')
-            
-            WebDriverWait(table_container_div, 10).until(EC.presence_of_element_located((By.TAG_NAME, 'table')))
-            table = table_container_div.find_element(By.TAG_NAME, 'table')
-            
-            WebDriverWait(table, 10).until(EC.presence_of_element_located((By.TAG_NAME, 'tbody')))
-            tbody = table.find_element(By.TAG_NAME, 'tbody')
-            
-            WebDriverWait(tbody, 10).until(EC.presence_of_all_elements_located((By.TAG_NAME, 'tr')))
-            rows = tbody.find_elements(By.TAG_NAME, 'tr')
-            
-            max_buy_amount = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//td[text()='Maximum Buy']")))
-            min_buy_amount = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//td[text()='Minimum Buy']")))
-            presale_address = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//td[text()='Presale Address']")))
+        options = Options()
+        options.headless = True
+        with webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options) as driver:
+            driver.get(url)
+            try:
+                WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 'ant-card-body')))
+                parent_div = driver.find_element(By.CLASS_NAME, 'ant-card-body')
+                WebDriverWait(parent_div, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 'table-container')))
+                table_container_div = parent_div.find_element(By.CLASS_NAME, 'table-container')
 
-            if len(rows) > 14:
-                target_row = rows[14]
-                
-                # Scroll to the target row
-                driver.execute_script("arguments[0].scrollIntoView();", target_row)
-                
-                # Wait for the content to load (adjust the sleep duration as needed)
-                sleep(2)
-                
-                WebDriverWait(target_row, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 'has-text-right')))
-                expected = target_row.find_element(By.CLASS_NAME, 'has-text-right')
-                target_row_html = expected.get_attribute('innerHTML')
-                target_row_html = target_row_html.split('(')[0].strip()
-                target_row_html = datetime.strptime(target_row_html, '%Y.%m.%d %H:%M')
-                END_TIME = int(target_row_html.timestamp())
+                WebDriverWait(table_container_div, 10).until(EC.presence_of_element_located((By.TAG_NAME, 'table')))
+                table = table_container_div.find_element(By.TAG_NAME, 'table')
 
-                if max_buy_amount and min_buy_amount and presale_address:
-                    MAX_BUY = max_buy_amount.find_element(By.XPATH, './following-sibling::td').text.strip().split(' ')[0]
-                    MIN_BUY = min_buy_amount.find_element(By.XPATH, './following-sibling::td').text.strip().split(' ')[0]
-                    PRESALE_ADDRESS = presale_address.find_element(By.XPATH, './following-sibling::td').text.strip()
+                WebDriverWait(table, 10).until(EC.presence_of_element_located((By.TAG_NAME, 'tbody')))
+                tbody = table.find_element(By.TAG_NAME, 'tbody')
 
-        finally:
-            driver.quit()
+                WebDriverWait(tbody, 10).until(EC.presence_of_all_elements_located((By.TAG_NAME, 'tr')))
+                rows = tbody.find_elements(By.TAG_NAME, 'tr')
+
+                max_buy_amount = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//td[text()='Maximum Buy']")))
+                min_buy_amount = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//td[text()='Minimum Buy']")))
+                presale_address = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//td[text()='Presale Address']")))
+
+                if len(rows) > 14:
+                    target_row = rows[14]
+
+                    # Scroll to the target row
+                    driver.execute_script("arguments[0].scrollIntoView();", target_row)
+
+                    # Wait for the content to load (adjust the sleep duration as needed)
+                    sleep(2)
+
+                    WebDriverWait(target_row, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 'has-text-right')))
+                    expected = target_row.find_element(By.CLASS_NAME, 'has-text-right')
+                    target_row_html = expected.get_attribute('innerHTML')
+                    target_row_html = target_row_html.split('(')[0].strip()
+                    target_row_html = datetime.strptime(target_row_html, '%Y.%m.%d %H:%M')
+                    END_TIME = int(target_row_html.timestamp())
+
+                    if max_buy_amount and min_buy_amount and presale_address:
+                        MAX_BUY = max_buy_amount.find_element(By.XPATH, './following-sibling::td').text.strip().split(' ')[0]
+                        MIN_BUY = min_buy_amount.find_element(By.XPATH, './following-sibling::td').text.strip().split(' ')[0]
+                        PRESALE_ADDRESS = presale_address.find_element(By.XPATH, './following-sibling::td').text.strip()
+            
+            except Exception as e:
+                print(e)
     
     return MIN_BUY, MAX_BUY, END_TIME, PRESALE_ADDRESS
-    # response = requests.get(url)
-    # if response.status_code == 200:
-    #     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-    #     sleep(2)
-    #     driver.get(url)
-    #     sleep(2)
-    #     soup = BeautifulSoup(driver.page_source, 'html.parser')
-    #     sleep(2)
-    #     parent_div = soup.find('div', class_='ant-card-body')
-    #     sleep(2)
-    #     if parent_div:
-    #         table_container_div = parent_div.find('div', class_='table-container')
-    #         sleep(2)
-    #         if table_container_div:
-    #             table = table_container_div.find('table')
-    #             sleep(2)
-    #             if table:
-    #                 tbody = table.find('tbody')
-    #                 sleep(2)
-    #                 if tbody:
-    #                     rows = tbody.find_all('tr')
-    #                     sleep(2)
-    #                     if len(rows) > 14:
-    #                         target_row = rows[14]
-    #                         sleep(2)
-    #                         if target_row:
-    #                             sleep(2)
-    #                             needed_td = target_row.find_all('td')[1]
-    #                             sleep(2)
-    #                             if needed_td:
-    #                                 needed_text = needed_td.get_text(strip=True)
-    #                                 datetime_str = needed_text.split('(')[0].strip()
-    #                                 datetime_obj = datetime.strptime(datetime_str, '%Y.%m.%d %H:%M')
-    #                             return datetime_obj
-
-    #     driver.close()
-    #     sleep(2)
-    #     return parent_div
-    # else:
-    #     return None
     
 if __name__ == '__main__':
     app.run(debug=True, port=8443)
